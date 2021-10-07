a630cb4a7d44096289efd408ff0d395ddd63395c
galter <ehgalter@gmail.com>
2021-10-07 21:18:17 +0100

    Initial Commit

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import express from 'express';
+import UserController from './app/controllers/UserController';
+
+const app = express();
+
+app.use(express.json())
+app.post('/users', UserController.store);
+
+app.listen(3333);
